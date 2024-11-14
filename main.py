5ea4ae164c9a55bab427cdec64fe92e38751d0a7
Debuger1024 <80024908+Debuger1024@users.noreply.github.com>
2024-11-15 03:39:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from src.app import app
 import os
+from dash import Dash
+from flask import Flask
 
-port = int(os.environ.get("PORT", 10000))
+server = Flask(__name__)
+app = Dash(__name__, server=server)
+
+port = int(os.getenv("PORT", 10000))
 
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0', port=port, debug=True)
-
+    server.run(host='0.0.0.0', port=port, debug=True)
