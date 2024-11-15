0a661ff779f753a61745d8657e446781a9d20676
Debuger1024 <80024908+Debuger1024@users.noreply.github.com>
2024-11-15 18:12:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ import os
 if __name__ == '__main__':
     app = create_app()
     server = app.server
-    port = int(os.environ.get("PORT", 10000))  # 默认端口设为8050
+    port = int(os.environ.get("PORT", 5000))  # 默认端口设为8050
     app.run_server(host="0.0.0.0", port=port)
 
