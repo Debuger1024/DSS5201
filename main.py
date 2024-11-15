301b0dd94a1e3e3b0a12f580ae2586e5d7e63d5f
Debuger1024 <80024908+Debuger1024@users.noreply.github.com>
2024-11-15 18:43:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,8 @@ import sys
 sys.path.append("src")  # 添加 src 路径到 Python 的搜索路径
 
 from app import create_app
-import os
 
 if __name__ == '__main__':
     app = create_app()
-    server = app.server
-    port = int(os.environ.get("PORT", 5000))  # 默认端口设为8050
-    app.run_server(host="0.0.0.0", port=port)
+    app.run_server(debug=True)
 
